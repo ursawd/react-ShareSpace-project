0495d8284ef00de78712c5a9583e6710a0307874
Phil Bailey <philbailey@msn.com>
2021-12-16 13:17:41 -0500

    add Button to NavBar

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Button(props) {
+  const style = {
+    backgroundColor: "#EEC320",
+  };
+  return (
+    <div>
+      <button style={style} class="btn btn-lg py-3 px-4 fs-3">
+        {props.btnText}
+      </button>
+    </div>
+  );
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./images/ss-logo.png";
+import Button from "./Button";
 
 export default function NavBar() {
   return (
@@ -20,28 +21,35 @@ export default function NavBar() {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
+          <ul class="navbar-nav ms-auto fs-5">
             <li class="nav-item">
-              <a class="nav-link " href="/#">
-                Home
+              <a class="nav-link  text-white" href="/">
+                Membership
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">
-                Features
+              <a class="nav-link text-white" href="/">
+                Suites
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">
-                Pricing
+              <a class="nav-link text-white" href="/">
+                Drop In
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link " href="/" tabindex="-1">
-                Disabled
+              <a class="nav-link  text-white" href="/">
+                Community
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  text-white" href="/">
+                Contact
               </a>
             </li>
           </ul>
+          <i class="fas fa-cart-plus fa-lg text-white me-3"></i>
+          <Button btnText="BOOK A TOUR" />
         </div>
       </div>
     </nav>
