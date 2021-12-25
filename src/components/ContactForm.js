6025bac96b4748e8fe8f017c5b791b7bc87d82d0
Phil Bailey <philbailey@msn.com>
2021-12-25 14:29:30 -0500

    add react form code

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,8 +1,18 @@
 import React from "react";
+import swal from "@sweetalert/with-react";
+
 export default class ContactForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      firstname: "",
+      lastname: "",
+      member: "",
+      tellus: "",
+      hearabout: "",
+      email: "",
+    };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,6 +26,15 @@ export default class ContactForm extends React.Component {
 
   handleSubmit(event) {
     console.log(this.state);
+    swal(`Form inputs retrieved from this.state:\n${JSON.stringify(this.state)}`);
+    this.setState({
+      firstname: "",
+      lastname: "",
+      member: "",
+      tellus: "",
+      hearabout: "",
+      email: "",
+    });
     event.preventDefault();
   }
 
@@ -44,13 +63,27 @@ export default class ContactForm extends React.Component {
             </div>
             <div className="col-12 row">
               <div className="form-group col-6">
-                <input type="text" className="form-control" id="firstName" />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstName"
+                  name="firstname"
+                  value={this.state.firstname}
+                  onChange={this.handleInputChange}
+                />
                 <label htmlFor="firstName" className="form-text text-muted">
                   First Name
                 </label>
               </div>
               <div className="form-group col-6">
-                <input type="text" className="form-control" id="lastName" />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  name="lastname"
+                  value={this.state.lastname}
+                  onChange={this.handleInputChange}
+                />
                 <label htmlFor="lastName" className="form-text text-muted">
                   Last Name
                 </label>
@@ -60,28 +93,56 @@ export default class ContactForm extends React.Component {
           <br />
           <div className="form-group">
             <label htmlFor="inputEmail">Email*</label>
-            <input type="email" className="form-control" id="inputEmail" />
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
           </div>
           <br />
           <label htmlFor="tellUs">Tell us something about yourself*</label>
           <div className="form-group">
-            <textarea className="form-control" id="tellUs" rows="4"></textarea>
+            <textarea
+              className="form-control"
+              id="tellUs"
+              rows="4"
+              name="tellus"
+              value={this.state.tellus}
+              onChange={this.handleInputChange}
+            ></textarea>
           </div>
           <br />
           <div className="form-group">
             <label htmlFor="hearAbout">How did you hear about us?</label>
-            <input type="text" className="form-control" id="hearAbout" />
+            <input
+              type="text"
+              className="form-control"
+              id="hearAbout"
+              name="hearabout"
+              value={this.state.hearabout}
+              onChange={this.handleInputChange}
+            />
           </div>
           <br />
           <div className="form-group">
             <label htmlFor="membershipSelect">Which membership are you interested in?*</label>
-            <select className="form-select" id="membershipSelect">
-              <option>1 Day / Week</option>
-              <option>2 Days / Week</option>
-              <option>24/7 Hot Desk</option>
-              <option>Virtual Office</option>
-              <option>Virtual Office+</option>
-              <option>I don't know yet</option>
+            <select
+              className="form-select"
+              id="membershipSelect"
+              name="member"
+              value={this.state.member}
+              onChange={this.handleInputChange}
+            >
+              <option member="">-make a selection-</option>
+              <option member="1 Day / Week">1 Day / Week</option>
+              <option member="2 Days / Week">2 Days / Week</option>
+              <option member="24/7 Hot Desk">24/7 Hot Desk</option>
+              <option member="Virtual Office">Virtual Office</option>
+              <option member="Virtual Office+">Virtual Office+</option>
+              <option member="I don't know yet">I don't know yet</option>
             </select>
           </div>
 
